1b4aa9b7171f818d20657719112bd0f51dca395b
Andrey Dovganyuk <andrewdovg@gmail.com>
2022-07-21 10:38:52 +0300

    typed text and button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Typed from 'react-typed'
 import './header.css'
 
 const Header = () => {
-  return <section className='header-wrapper'></section>
+  return (
+    <section className='header-wrapper'>
+      <div className='main-info'>
+        <h1>web development and website promotion</h1>
+        <Typed
+          className='typed-text'
+          strings={[
+            'Web Design',
+            'Web Development',
+            'Facebook Ads SMM',
+            'Google Adwords',
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href='#' className='btn-main-offer'>
+          contact me
+        </a>
+      </div>
+    </section>
+  )
 }
 
 export default Header
